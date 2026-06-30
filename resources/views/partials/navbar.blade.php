ccd0b874a4d3e9f6d604b36a879097909029c827
shahzaibriaz69 <shahzaibraiz0023@gmail.com>
2026-06-30 17:58:44 +0500

    update nabar setting

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,27 +1,37 @@
-<nav class="navbar navbar-expand-lg custom-navbar fixed-top">
+<nav class="navbar navbar-expand-lg custom-navbar fixed-top" id="mainNav">
     <div class="container">
+
         <!-- Logo -->
         <a class="navbar-brand" href="{{ url('/') }}">
-            <img src="{{ asset('assets/images/digi-iconics-logo.png') }}" alt="DIGI-ICONICS Logo" class="logo-img">
+            <img src="{{ asset('assets/images/digi-iconics-logo.png') }}" alt="DIGI-ICONICS" class="logo-img">
         </a>
 
-
-        <button class="navbar-toggler custom-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainNavbar"
-            aria-controls="mainNavbar" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="fas fa-bars"></span>
+        <!-- Mobile Toggler -->
+        <button class="navbar-toggler custom-toggler" type="button" data-bs-toggle="collapse"
+            data-bs-target="#mainNavbar" aria-controls="mainNavbar" aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span class="toggler-icon">
+                <span></span>
+                <span></span>
+                <span></span>
+            </span>
         </button>
 
         <div class="collapse navbar-collapse" id="mainNavbar">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0 align-items-center">
+
                 <li class="nav-item">
-                    <a class="nav-link active" href="{{ url('/') }}">Home</a>
+                    <a class="nav-link {{ request()->is('/') ? 'active' : '' }}" href="{{ url('/') }}">Home</a>
                 </li>
+
                 <li class="nav-item">
-                    <a class="nav-link" href="#">About</a>
+                    <a class="nav-link {{ request()->is('about') ? 'active' : '' }}"
+                        href="{{ url('/about') }}">About</a>
                 </li>
 
                 <li class="nav-item dropdown has-mega-menu">
-                    <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">
+                    <a class="nav-link dropdown-toggle {{ request()->is('services*') ? 'active' : '' }}" href="#"
+                        data-bs-toggle="dropdown" aria-expanded="false">
                         Services <i class="fas fa-chevron-down ms-1 style-icon"></i>
                     </a>
                     <div class="dropdown-menu mega-menu-wrapper p-4">
@@ -29,7 +39,9 @@
 
                             <div class="col-md-4 mega-column-border">
                                 <div class="mega-header-wrapper">
-                                    <i class="fas fa-laptop-code text-indigo menu-main-icon"></i>
+                                    <div class="mega-icon-box mega-icon-indigo">
+                                        <i class="fas fa-laptop-code"></i>
+                                    </div>
                                     <h6 class="mega-title">Development</h6>
                                 </div>
                                 <div class="mega-links-container">
@@ -50,7 +62,9 @@
 
                             <div class="col-md-4 mega-column-border">
                                 <div class="mega-header-wrapper">
-                                    <i class="fas fa-rocket text-warning menu-main-icon"></i>
+                                    <div class="mega-icon-box mega-icon-warning">
+                                        <i class="fas fa-rocket"></i>
+                                    </div>
                                     <h6 class="mega-title">Marketing & SEO</h6>
                                 </div>
                                 <div class="mega-links-container">
@@ -71,7 +85,9 @@
 
                             <div class="col-md-4">
                                 <div class="mega-header-wrapper">
-                                    <i class="fas fa-bezier-curve text-cyan menu-main-icon"></i>
+                                    <div class="mega-icon-box mega-icon-cyan">
+                                        <i class="fas fa-bezier-curve"></i>
+                                    </div>
                                     <h6 class="mega-title">Creative Studio</h6>
                                 </div>
                                 <div class="mega-links-container">
@@ -83,7 +99,6 @@
                                         <i class="fas fa-wand-magic-sparkles text-pink-soft ref-icon"></i>
                                         <span>Branding Services</span>
                                     </a>
-
                                     <a class="dropdown-item" href="#">
                                         <i class="fab fa-figma text-figma ref-icon"></i>
                                         <span>UI/UX Design</span>
@@ -95,16 +110,20 @@
                     </div>
                 </li>
 
+                <li class="nav-item">
+                    <a class="nav-link {{ request()->is('portfolio*') ? 'active' : '' }}"
+                        href="{{ url('/portfolio') }}">Portfolio</a>
+                </li>
 
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Portfolio</a>
+                    <a class="nav-link {{ request()->is('blog*') ? 'active' : '' }}"
+                        href="{{ url('/blog') }}">Blog</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Blog</a>
-                </li>
+
                 <li class="nav-item ms-lg-3">
-                    <a class="btn btn-proposal" href="#">Get Proposal</a>
+                    <a class="btn btn-proposal" href="{{ url('/contact') }}">Get Proposal</a>
                 </li>
+
             </ul>
         </div>
     </div>
